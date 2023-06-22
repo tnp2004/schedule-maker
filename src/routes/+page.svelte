5becfcc37baadb2d032791e5a34ae16636e97905
tnp2004 <iceandiceisice2004@gmail.com>
2023-06-22 15:13:29 +0700

    set up project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import html2canvas from 'html2canvas';
+
+    const downloadImage = () => {
+        const imgTarget = document.getElementById("something") as HTMLElement
+        const imgName = "schedule"
+
+        html2canvas(imgTarget).then((canvas) => {
+            const base64image = canvas.toDataURL("image/png")
+            var anchor = document.createElement("a")
+            anchor.setAttribute("href", base64image)
+            anchor.setAttribute("download", imgName)
+            anchor.click()
+            anchor.remove()
+        })
+    }
+</script>
+
+<h1>Welcome to Schedule Maker</h1>
+<div id="something">
+	<h3>hello world</h3>
+	<p>i have a pen</p>
+</div>
+<button on:click={downloadImage}>Download</button>
