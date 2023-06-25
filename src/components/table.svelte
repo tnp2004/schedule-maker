9130587f61fa95eaed96cb200635cb47fe52888c
tnp2004 <iceandiceisice2004@gmail.com>
2023-06-25 17:33:33 +0700

    sort task by the Time

--- src/components/table.svelte
+++ src/components/table.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { Subject, Task } from '../../types';
 	const timeRange = 1080; // min per day
-	let a = 30;
 
 	export let schedulesList: Task;
 
@@ -31,7 +30,7 @@
 	};
 </script>
 
-<div class="w-5/6 h-fit border-2 mx-auto my-6 grid grid-cols-1 p-2" id="scheduleTable">
+<div class="w-5/6 h-fit border-4 rounded mx-auto my-6 grid grid-cols-1 p-2" id="scheduleTable">
 	<div class="flex border-b-2 last:border-b-0 h-fit">
 		<div class="border-r-2 flex justify-center items-center w-32 mr-2">Mon</div>
 		{#each schedulesList.Monday as task}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,10 @@
 	import Form from '../components/form.svelte';
 	import type { DispatchEvent, Task, TaskData } from '../../types';
 	import Table from '../components/table.svelte';
-	const mock = [{"id":"1","subject":"Math","room":"OP293","time":{"from":"15:02","to":"17:02"}},{"id":"asd","subject":"xcb","room":"fdb","time":{"from":"15:58","to":"19:58"}},{"id":"asd","subject":"cx","room":"db","time":{"from":"14:57","to":"14:58"}}]
+	const mock = [
+		{ id: 'a', subject: 'a', room: 'a', time: { from: '17:08', to: '18:08' } },
+		{ id: 'b', subject: 'b', room: 'b', time: { from: '19:08', to: '20:08' } }
+	];
 	const schedulesList: Task = {
 		Monday: [...mock],
 		Tuesday: [],
@@ -11,12 +14,22 @@
 		Thursday: [],
 		Friday: [],
 		Saturday: [],
-		Sunday: [],
+		Sunday: []
 	};
 
 	const addSchedule = (event: DispatchEvent): void => {
-		const { task: { day, id, subject, room, time } }: { task: TaskData } = event.detail;
-		schedulesList[day] = [{ id, subject, room, time }, ...schedulesList[day]]
+		const {
+			task: { day, id, subject, room, time }
+		}: { task: TaskData } = event.detail;
+		const schedule = { id, subject, room, time };
+		for (let i = 0; i < schedulesList[day].length; i++) {
+			if (!compareTime(schedule.time.from, schedulesList[day][i].time.from)) {
+				schedulesList[day].splice(i, 0, schedule)
+				schedulesList[day] = schedulesList[day]
+				return;
+			}
+		}
+		schedulesList[day] = [...schedulesList[day], schedule];
 	};
 
 	const downloadImage = () => {
@@ -33,13 +46,34 @@
 		});
 	};
 
-	
+	const compareTime = (sch: string, curr: string): Boolean => {
+		const [hours1, minutes1] = sch.split(':').map(Number);
+		const [hours2, minutes2] = curr.split(':').map(Number);
+
+		if (hours1 > hours2) {
+			return true;
+		} else if (hours1 < hours2) {
+			return false;
+		} else {
+			if (minutes1 > minutes2) {
+				return true;
+			} else if (minutes1 < minutes2) {
+				return false;
+			} else {
+				// time equal
+				return false;
+			}
+		}
+	};
 </script>
 
 <h1 class="text-rose-600">Welcome to Schedule Maker</h1>
 {JSON.stringify(schedulesList)}
 <Form on:add={addSchedule} />
-<Table schedulesList={schedulesList} />
+<Table {schedulesList} />
 <div class="w-fit mx-auto my-2">
-	<button class="border-2 p-1 w- hover:bg-gradient-to-r from-slate-100 to-slate-200" on:click={downloadImage}>Download</button>
+	<button
+		class="border-2 p-1 w- hover:bg-gradient-to-r from-slate-100 to-slate-200"
+		on:click={downloadImage}>Download</button
+	>
 </div>
